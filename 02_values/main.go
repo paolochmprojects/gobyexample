8d9667face795c100223410d17bb1fd74fffb30e
paolochmprojects <paolo.dev.projects@gmail.com>
2024-03-28 19:00:16 -0500

    [ADD] values exec

---
+++ 02_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// concadenaciones de strings
+	fmt.Println("go" + "lang")
+
+	// concadenaciones de sttings e integers
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// imprecion de booleanos
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!false)
+}
